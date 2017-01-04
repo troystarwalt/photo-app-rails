3b33e679c3ff3f6035c3ce7be2d6f60f282c7cab
Troy Starwalt <troy.starwalt@gmail.com>
2017-01-03 21:51:33 -0500

    nothing to see here

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    @most_recent = Photo.last_five
+    @most_recent = Photo.last_eight
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ApplicationRecord
   mount_uploader :image, ImageUploader
   mount_uploaders :images, ImageUploader
-  scope :last_five, -> { order("created_at desc").last(5) }
+  scope :last_eight, -> { order("created_at desc").last(8) }
   validates :title, :description, :image, :date_taken, :presence => true
 
 end
